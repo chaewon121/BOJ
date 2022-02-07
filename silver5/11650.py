912bf06c1f077e250c12638bd783d8c8863810db
채워니 <cw121@naver.com>
2022-02-08 01:56:58 +0900

    좌표 정렬하기

---
+++ silver5/11650.py
@@ -0,0 +1,12 @@
+n=int(input())
+a=[[0]*2 for _ in range(n)]
+
+for i in range(n):
+  x,y=map(int,input().split())
+  a[i][0]=x
+  a[i][1]=y
+
+a.sort(key=lambda x:(x[0],x[1]))
+
+for i in range(n):
+  print(a[i][0],a[i][1])
